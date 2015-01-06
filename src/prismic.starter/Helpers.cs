f4e8b30c79beaeaeea711b997b7bbd985f8a208c
Erwan Loisant <eloisant@gmail.com>
2015-01-06 13:47:12 +0100

    Remove dead code

--- src/prismic.starter/Helpers.cs
+++ src/prismic.starter/Helpers.cs
@@ -11,38 +11,6 @@ using prismic;
 
 namespace prismic.mvc.starter
 {
-	public static class NetHelper
-	{
-		public static string Post(this string url, IDictionary<string, string> values)
-		{	
-			var data = new NameValueCollection();
-			foreach (var item in values) {
-				data.Add (item.Key, item.Value);
-			}
-
-			using(var client = new WebClient())
-			{
-				byte[] responsebytes = client.UploadValues(url, "POST", data);
-				return Encoding.UTF8.GetString(responsebytes); // WebException TODO :================
-			}
-		}
-
-
-		public static string BuildUrlWith(this string baseUrl, IDictionary<string, string> values)
-		{
-			var uri = new System.Uri (baseUrl, UriKind.Absolute);
-			var httpValueCollection = HttpUtility.ParseQueryString (uri.Query);
-			var nameValueCollection = new NameValueCollection ();
-			foreach (var item in values) {
-				nameValueCollection.Add (item.Key, item.Value); // TODO: encode UTF-8 / use Uri
-			}
-			httpValueCollection.Add (nameValueCollection);
-			var queryString = httpValueCollection.ToString ();
-			var builder = new UriBuilder (uri);
-			builder.Query = queryString;
-			return builder.ToString ();
-		}
-	}
 
 	public static class RequestContextExt
 	{
@@ -51,6 +19,7 @@ namespace prismic.mvc.starter
 			src.ToList().ForEach(x => { dest[x.Key] = x.Value; });
 			return dest;
 		}
+
 		public static string RouteUrlFor(this RequestContext requestContext, string action, string controller, object routeValues)
 		{
 			var values = 
@@ -60,31 +29,8 @@ namespace prismic.mvc.starter
 			var helper = new UrlHelper(requestContext);
 			return helper.RouteUrl (values);
 		}
-		public static string AbsoluteRouteUrlFor(this RequestContext requestContext, string action, string controller, object routeValues)
-		{
-			var helper = new UrlHelper(requestContext);
-			var scheme = requestContext.HttpContext.Request.Url.Scheme;
-			return helper.Action (action, controller, routeValues, scheme);
-		}
-	}
-
-	public static class NameValueCollectionExt
-	{
-		public static bool TryGetValue(this NameValueCollection settings, string settingKey, out string value)
-		{
-			value = settings[settingKey];
-			return !string.IsNullOrEmpty (value);
-		}
-		public static string GetOrThrow(this NameValueCollection settings, string settingKey)
-		{
-			var setting = settings[settingKey];
-			if (!string.IsNullOrEmpty (setting)) 
-			{
-				return setting;
-			}
-			throw new KeyNotFoundException ("key is missing : " + settingKey);
-		}
 
 	}
+
 }
 
