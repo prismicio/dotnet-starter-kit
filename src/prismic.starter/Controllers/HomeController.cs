ebce218f542c467079f5887ec64b0878a69d1e01
Erwan Loisant <eloisant@gmail.com>
2015-01-06 12:08:36 +0100

    Convert all actions to async

--- src/prismic.starter/Controllers/HomeController.cs
+++ src/prismic.starter/Controllers/HomeController.cs
@@ -11,30 +11,22 @@ namespace prismic.mvc.starter.Controllers
 	{
 		public async Task<ActionResult> Index (int page = 1)
 		{
-            var api = await PrismicApiHome.FromConfig().Get();
-            HttpCookie previewCookie = HttpContext.Request.Cookies.Get(prismic.Api.PREVIEW_COOKIE);
-            string maybeRef = (previewCookie != null && previewCookie.Value != "")
-                ? previewCookie.Value
-                : api.Master.Reference;
-
-            var ctx = new PrismicContext(api, maybeRef,
-                                PrismicLinkResolver.Get(api, maybeRef, ControllerContext.RequestContext));
-			var response = await api.Form("everything").Ref (ctx.MaybeRef).PageSize (10).Page (page)
-				.Submit ();
+            var ctx = await getContext();
+			var response = await ctx.Api.Form("everything").Ref (ctx.MaybeRef).PageSize (10).Page (page).Submit ();
 			ViewBag.Title = "All documents";
 			var model = new PrismicResponse(ctx, response);
 			return View (model);
 		}
 
-		[PrismicContext("ctx")]
-		public ActionResult Detail(PrismicContext ctx, string id, string slug)
+		public async Task<ActionResult> Detail(string id, string slug)
 		{
-			var futureResponse = ctx.Api.Form("everything")
+            var ctx = await getContext();
+            var response = await ctx.Api.Form("everything")
 				.Query(Predicates.at("document.id", id))
 				.Ref (ctx.MaybeRef)
 				.Submit (); 
 
-			var document = futureResponse.Result.Results.FirstOrDefault ();
+			var document = response.Results.FirstOrDefault ();
 			if (document != null && document.Slug == slug) {
 				ViewBag.Title = "Document detail " + slug;
 				return View (new PrismicDocument (ctx, document));
@@ -53,29 +45,29 @@ namespace prismic.mvc.starter.Controllers
 			return RedirectToAction ("PageNotFound");
 		}
 
-		[PrismicContext("ctx")]
-		public ActionResult PageNotFound(PrismicContext ctx)
+		public async Task<ActionResult> PageNotFound()
 		{
+            var ctx = await getContext();
 			ViewBag.Title = "Document not found";
 			return View (new PrismicViewModel(ctx));
 		}
 
-		[PrismicContext("ctx")]
-		public ActionResult Search(PrismicContext ctx, string q, int page = 1)
+		public async Task<ActionResult> Search(string q, int page = 1)
 		{
+            var ctx = await getContext();
 			var query = string.IsNullOrWhiteSpace (q) ? string.Empty : q;
-			var futureResponse = ctx.Api.Form("everything")
+			var response = await ctx.Api.Form("everything")
 				.Query(Predicates.fulltext("document", query))
 				.Ref (ctx.MaybeRef).PageSize (10).Page (page)
 				.Submit (); 
 
 			ViewBag.Title = "Search results";
-			return View (new PrismicSearchResponse(ctx, futureResponse.Result, q));
+			return View (new PrismicSearchResponse(ctx, response, q));
 		}
 
-		[PrismicContext("ctx")]
-		public ActionResult Preview(PrismicContext ctx, string token)
+		public async Task<ActionResult> Preview(string token)
 		{
+            var ctx = await getContext();
 			string url = ctx.Api.PreviewSession (token, ctx.LinkResolver, "/").Result;
 			var cookie = new HttpCookie (prismic.Api.PREVIEW_COOKIE, token);
 			cookie.Expires = DateTime.Now.AddMinutes (30);
@@ -83,6 +75,15 @@ namespace prismic.mvc.starter.Controllers
 			return Redirect (url);
 		}
 
+        private async Task<PrismicContext> getContext()
+        {
+            var api = await PrismicApiHome.FromConfig().Get();
+            HttpCookie previewCookie = HttpContext.Request.Cookies.Get(prismic.Api.PREVIEW_COOKIE);
+            string maybeRef = (previewCookie != null && previewCookie.Value != "")
+                ? previewCookie.Value
+                : api.Master.Reference;
+            return new PrismicContext(api, maybeRef, PrismicLinkResolver.Get(api, maybeRef, ControllerContext.RequestContext));
+        }
 	}
 
 
